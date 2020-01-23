52f3c99c8154417869a2f21fa24c4d65b7df2b18
mar1n <30738216+mar1n@users.noreply.github.com>
2020-01-23 23:50:50 +0000

    test practice

---
+++ test/Welcome.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import chai from 'chai';
+import { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme'
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Welcome from '../src/Welcome';
+configure({ adapter: new Adapter() });
+
+import { shallow, mount, render } from 'enzyme';
+
+describe('Welcome', () => {
+	//when
+
+    it('Displays a welcome message', () => {
+        const welcome = shallow(<Welcome />);
+
+        //then
+        expect(welcome.text('.test')).to.equal('Welcome to React!');
+    });
+});
---
+++ test/Welcome2.test.js
@@ -0,0 +1,5 @@
+describe('Welcome', () => {
+    it('Welcome returns welcome message in 2 paragraphs', () => {
+        
+    })
+})
