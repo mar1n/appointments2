d60007a02f0e7b620af2b0f062b2a52601df330d
mar1n <30738216+mar1n@users.noreply.github.com>
2020-01-25 21:12:56 +0000

    add another test and rename all test file

---
+++ test/PorpsDisplayuspresidents.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import chai from 'chai';
+import { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme'
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import President from '../src/President';
+
+configure({ adapter: new Adapter() });
+
+import { shallow, mount, render } from 'enzyme';
+
+describe('Pass props and display them', () => {
+    it('President display details for Bill Clinton', () => {
+        const president = shallow(<President firstName='Bill' lastName='Clinton' />);
+
+        expect(president.find('.firstName').text()).to.equal('First name: Bill');
+        expect(president.find('.lastName').text()).to.equal('Last name: Clinton');
+    });
+});
