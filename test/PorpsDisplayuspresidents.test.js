4f2f0dd0b80acf1c06b19b71928f9b52546aef7f
mar1n <30738216+mar1n@users.noreply.github.com>
2020-01-25 21:52:04 +0000

    another test

--- test/PorpsDisplayuspresidents.test.js
+++ test/PorpsDisplayuspresidents.test.js
@@ -18,4 +18,10 @@ describe('Pass props and display them', () => {
         expect(president.find('.firstName').text()).to.equal('First name: Bill');
         expect(president.find('.lastName').text()).to.equal('Last name: Clinton');
     });
+    it('President display details for Barack Obama', () => {
+        const president = shallow(<President firstName='Barack' lastName='Obama' />);
+
+        expect(president.find('.firstName').text()).to.equal('First name: Barack');
+        expect(president.find('.lastName').text()).to.equal('Last name: Obama');
+    });
 });
---
+++ test/PropsShowplayerdetails.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import chai from 'chai';
+import { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme'
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Player from '../src/Playerdetails';
+
+configure({ adapter: new Adapter() });
+
+import { shallow, mount, render } from 'enzyme';
+
+describe('Pass props and display them', () => {
+    it('Player displays score details for Andrea', () => {
+        const person = { name: 'Andrea', score: 65 };
+
+        const player = shallow(<Player person={person} /> );
+
+        expect(player.find('.name').text()).to.equal('Name: Andrea');
+        expect(player.find('.score').text()).to.equal('Score: 65');
+    });
+    
+})
